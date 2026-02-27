817f9e28ff675acb82dd40ba2f53e38e43043680
Mazen Khaled Mohamed <mazen.khaled2409@gmail.com>
2026-02-27 23:12:30 +0200

    Experiment 1: Logistic Regression

--- src/train.py
+++ src/train.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import joblib
+from sklearn.linear_model import LogisticRegression
 
 train = pd.read_csv("data/processed/train.csv")
 
 X = train.iloc[:, :-1]
 y = train.iloc[:, -1]
 
+model = LogisticRegression(max_iter=1000)
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
