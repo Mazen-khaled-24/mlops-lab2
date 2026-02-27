1c00b1aa6e45ac5323cc44334e323dab0ab65873
Mazen Khaled Mohamed <mazen.khaled2409@gmail.com>
2026-02-27 23:24:02 +0200

    Experiment 2: Random Forest

--- src/train.py
+++ src/train.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import joblib
+from sklearn.ensemble import RandomForestClassifier
 
 train = pd.read_csv("data/processed/train.csv")
 
 X = train.iloc[:, :-1]
 y = train.iloc[:, -1]
 
+model = RandomForestClassifier(random_state=42)
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
